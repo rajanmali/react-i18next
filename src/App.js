32966d2a8f184403e3d0a3d8b93a9ae75cf73dc0
Rajan Mali <mysteriouskiller50@gmail.com>
2021-07-06 13:28:30 +0545

    added basic setup for i18next and i18next lang detection

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import i18n from 'i18next';
+import { useTranslation, initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+// Utils
+import languageDetectorOptions from './utils/languageDetectorOptions';
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector) // passes i18n down to react-i18next
+  .init({
+    // the translations
+    // (tip move them in a JSON file and import them,
+    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+    resources: {
+      en: {
+        translation: {
+          'Welcome to React': 'Welcome to React and react-i18next',
+        },
+      },
+      fr: {
+        translation: {
+          'Welcome to React': 'Bienvenue a React et react-i18next',
+        },
+      },
+    },
+    detection: languageDetectorOptions,
+    fallbackLng: 'en',
+  });
+
 const App = () => {
-  return <div className="App">Hello World</div>;
+  const { t } = useTranslation();
+  return <h2>{t('Welcome to React')}</h2>;
 };
 
 export default App;
