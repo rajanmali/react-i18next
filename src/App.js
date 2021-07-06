8cfc2534e6e2a744a0552beaca6b8a5e0ea4b181
Rajan Mali <mysteriouskiller50@gmail.com>
2021-07-06 13:41:03 +0545

    added i18next-http-backend to fetch translation files dynamically

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
 import i18n from 'i18next';
 import { useTranslation, initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
 
 // Utils
 import languageDetectorOptions from './utils/languageDetectorOptions';
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
-  .use(LanguageDetector) // passes i18n down to react-i18next
+  .use(LanguageDetector) // detect the browser language automatically
+  .use(HttpApi) // load resources from a translation.json files
   .init({
-    // the translations
-    // (tip move them in a JSON file and import them,
-    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
-    resources: {
-      en: {
-        translation: {
-          'Welcome to React': 'Welcome to React and react-i18next',
-        },
-      },
-      fr: {
-        translation: {
-          'Welcome to React': 'Bienvenue a React et react-i18next',
-        },
-      },
-    },
-    detection: languageDetectorOptions,
     fallbackLng: 'en',
+    detection: languageDetectorOptions,
+    backend: {
+      loadPath: '/assets/locales/{{lng}}/translation.json',
+    },
+    react: { useSuspense: false },
   });
 
 const App = () => {
   const { t } = useTranslation();
-  return <h2>{t('Welcome to React')}</h2>;
+  return <h2>{t('welcome_to_react')}</h2>;
 };
 
 export default App;
