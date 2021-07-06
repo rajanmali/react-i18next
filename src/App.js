9e936a20c3843f5daf0bf05ead8cbf5fba73e557
Rajan Mali <mysteriouskiller50@gmail.com>
2021-07-06 15:07:49 +0545

    changes to dropdown to show language header

--- src/App.js
+++ src/App.js
@@ -36,15 +36,20 @@ const App = () => {
           </Dropdown.Toggle>
 
           <Dropdown.Menu>
+            <span className="dropdown-item-text">{t('language')}</span>
             {languagesArray.map(({ code, name, countryCode }) => (
               <Dropdown.Item key={countryCode}>
                 <li>
                   <button
                     className={'dropdown-item'}
                     onClick={() => i18next.changeLanguage(code)}
+                    disabled={code === currentLanguageCode}
                   >
                     <span
                       className={`flag-icon flag-icon-${countryCode} mx-2`}
+                      style={{
+                        opacity: code === currentLanguageCode ? 0.5 : 1,
+                      }}
                     ></span>
                     {name}
                   </button>
